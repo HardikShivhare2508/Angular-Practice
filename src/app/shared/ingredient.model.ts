9ecd2abee32b916d16bc8586415c8e05d34fd278
Hardik Shivhare <hardik.shivhare@gmail.com>
2019-06-19 13:32:25 -0400

    First Commit

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,19 @@
+export class Ingredient {
+    public name: string;
+    public amount: number;
+
+    constructor(name: string, amount: number){
+        this.name = name;
+        this.amount = amount;
+    }
+}
+
+
+
+// Another Way of defining class and construnctor 
+
+// export Class Ingredient{
+//     constructor(public name: string, public amount: number){
+
+//     }
+// }
